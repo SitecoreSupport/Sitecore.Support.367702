28cbc00a8d85a971d822259d2317ed976bc9cd9e
Muhammad Yasir Nawaz <myn@sitecore..net>
2017-05-25 14:10:03 +0800

    commit

---
+++ src/Sitecore.Support.367702/SiteResolvingPatcher.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Links;
+using Sitecore.Pipelines;
+using System;
+using System.Reflection;
+
+
+namespace Sitecore.Support
+{
+    public class SiteResolvingPatcher
+    {
+        public void Process(PipelineArgs args)
+        {
+            LinkProvider provider = LinkManager.Provider;
+            FieldInfo field = typeof(LinkProvider).GetField("defaultUrlOptions", BindingFlags.Instance | BindingFlags.NonPublic);
+            UrlOptions urlOptions = field.GetValue(provider) as UrlOptions;
+            urlOptions.SiteResolving = Settings.Rendering.SiteResolving;
+        }
+    }
+}
