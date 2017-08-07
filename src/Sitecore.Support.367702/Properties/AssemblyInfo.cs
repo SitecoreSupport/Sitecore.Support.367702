18956b47eb999b437cee7c689fc68fb5f1ce1b49
Eugene Oparyshev <eop@sitecore.net>
2017-08-07 19:43:54 +0300

    Github patch

---
+++ src/Sitecore.Support.367702/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.367702")]
+[assembly: AssemblyProduct("Sitecore.Support.367702")]
+[assembly: ComVisible(false)]
